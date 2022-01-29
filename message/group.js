ca8960e070c50e57b460142a99d1c9513ba5ee3e
NotStonks21 <90654639+NotStonks21@users.noreply.github.com>
2022-01-29 11:39:48 -0500

    Update group.js

--- message/group.js
+++ message/group.js
@@ -20,10 +20,10 @@ let shortpc = await axios.get(`https://tinyurl.com/api-create.php?url=${ppuser}`
 let chat = global.db.data.chats[anu.id] || {}
 
 if (anu.action == 'add') {
-let teks = (chat.setWelcome || '*Selamat Datang Di Group @subject*\n─────────────────\n*Nama : @user*\n*Pada : @tanggal*\n*Jangan Lupa Baca Rules Group*\n─────────────────\n@desc').replace(/@subject/g, metadata.subject).replace(/@user/g, `@${num.split('@')[0]}`).replace(/@tanggal/g, `${tanggal(new Date())}`).replace(/@desc/g, `${metadata.desc}`)
+let teks = (chat.setWelcome || '*Bienvenidos al Grupo @subject*\n─────────────────\n*Name : @user*\n*En : @tanggal*\n*No olvides leer el grupo de reglas*\n─────────────────\n@desc').replace(/@subject/g, metadata.subject).replace(/@user/g, `@${num.split('@')[0]}`).replace(/@tanggal/g, `${tanggal(new Date())}`).replace(/@desc/g, `${metadata.desc}`)
 dasha.sendMessage(anu.id, { image: await getBuffer(`https://hardianto.xyz/api/welcome4?profile=${shortpc.data}&name=${num.split("@")[0]}`), caption: teks, mentions : [num]})
 } else if (anu.action == 'remove') {
-let teks = (chat.setLeave || '*Sayonara* 👋\n─────────────────\n*Nama : @user*\n*Pada : @tanggal*\n\nTelah Meninggalkan Group @subject').replace(/@subject/g, metadata.subject).replace(/@user/g, `@${num.split('@')[0]}`).replace(/@tanggal/g, `${tanggal(new Date())}`).replace(/@desc/g, `${metadata.desc}`)
+let teks = (chat.setLeave || '*adios* 👋\n─────────────────\n*Name : @user*\n*Pada : @tanggal*\n\nHan abandonado el grupo @subject').replace(/@subject/g, metadata.subject).replace(/@user/g, `@${num.split('@')[0]}`).replace(/@tanggal/g, `${tanggal(new Date())}`).replace(/@desc/g, `${metadata.desc}`)
 dasha.sendMessage(anu.id, { image: await getBuffer(`https://hardianto.xyz/api/goodbye4?profile=${shortpc.data}&name=${num.split("@")[0]}`), caption: teks, mentions : [num]})
 }
 }  
